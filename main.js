5d9e88b5c16330294c8b51a1e26974ecc94065cd
a.riabinin <a.riabinin@notamedia.com.ru>
2025-05-16 18:48:51 +0300

    upd

--- main.js
+++ main.js
@@ -1,20 +1,19 @@
 import Swiper from 'swiper';
+import { Autoplay } from 'swiper/modules';
 import 'swiper/css';
 
 import lightGallery from 'lightgallery';
-import lgThumbnail from 'lightgallery/plugins/thumbnail'
-import lgZoom from 'lightgallery/plugins/zoom'
 import lgVideo from 'lightgallery/plugins/video'
 
 const swiper = new Swiper('.swiper', {
+  modules: [
+    Autoplay
+  ],
   loop: true,
-  pagination: {
-    el: '.swiper-pagination',
-  },
   slidesPerView: "auto",
   spaceBetween: 30,
   autoplay: {
-    delay: 5000,
+    delay: 1000,
   },
   // autoHeight: true
 });
